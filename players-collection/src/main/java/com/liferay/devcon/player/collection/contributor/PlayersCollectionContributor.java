b5fb1a0de0cc2bf2a7382ee9f936921886bd87c7
Pavel Savinov <pavel.savinov@liferay.com>
2019-10-14 08:18:28 +0200

    Implement players collection contributor

---
+++ players-collection/src/main/java/com/liferay/devcon/player/collection/contributor/PlayersCollectionContributor.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.devcon.player.collection.contributor;
+
+import com.liferay.fragment.contributor.BaseFragmentCollectionContributor;
+import com.liferay.fragment.contributor.FragmentCollectionContributor;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(service = FragmentCollectionContributor.class)
+public class PlayersCollectionContributor
+	extends BaseFragmentCollectionContributor {
+
+	@Override
+	public String getFragmentCollectionKey() {
+		return "DEVCON_PLAYERS";
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.devcon.player.collection.contributor)"
+	)
+	private ServletContext _servletContext;
+
+}
