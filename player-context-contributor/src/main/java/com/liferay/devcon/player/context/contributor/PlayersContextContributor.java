12ef76631e002b8a823246f88ba6b65c6240c514
Pavel Savinov <pavel.savinov@liferay.com>
2019-10-25 12:39:45 +0200

    Inject equipment item renderer util to the FreeMarker context

--- player-context-contributor/src/main/java/com/liferay/devcon/player/context/contributor/PlayersContextContributor.java
+++ player-context-contributor/src/main/java/com/liferay/devcon/player/context/contributor/PlayersContextContributor.java
@@ -16,6 +16,13 @@ package com.liferay.devcon.player.context.contributor;
 
 import com.liferay.portal.kernel.template.TemplateContextContributor;
 
+import com.liferay.devcon.player.equipment.entity.Ball;
+import com.liferay.devcon.player.equipment.entity.Equipment;
+import com.liferay.devcon.player.equipment.entity.Uniform;
+import com.liferay.devcon.player.equipment.renderer.EquipmentRendererUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -40,6 +47,18 @@ public class PlayersContextContributor implements TemplateContextContributor 	{
 		HttpServletRequest httpServletRequest) {
 
 		contextObjects.put("random", new Random());
+
+		List<Equipment> equipment = new ArrayList<>();
+
+		equipment.add(new Ball());
+		equipment.add(new Uniform());
+
+		contextObjects.put("equipment", equipment);
+		contextObjects.put(
+			"equipmentRendererUtil", _equipmentRendererUtil);
 	}
+
+	@Reference
+	private EquipmentRendererUtil _equipmentRendererUtil;
 
 }
