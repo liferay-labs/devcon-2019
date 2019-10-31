ea3e8bbb64d1ce51420f460c985b3058878c1fad
Pavel Savinov <pavel.savinov@liferay.com>
2019-10-31 12:26:37 +0100

    Add configuration support for dynamically rendered player

--- player-fragment-renderer/src/main/java/com/liferay/devcon/player/fragment/renderer/PlayerFragmentRenderer.java
+++ player-fragment-renderer/src/main/java/com/liferay/devcon/player/fragment/renderer/PlayerFragmentRenderer.java
@@ -15,8 +15,15 @@
 package com.liferay.devcon.player.collection.contributor;
 
 import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.fragment.util.FragmentEntryConfigUtil;
+import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;	
 import java.util.Locale;
@@ -40,6 +47,54 @@ public class PlayerFragmentRenderer implements FragmentRenderer {
 	}
 
 	@Override
+	public String getConfiguration(
+		FragmentRendererContext fragmentRendererContext) {
+
+		return JSONUtil.put(
+			"fieldSets",
+			JSONUtil.putAll(
+				JSONUtil.put(
+					"fields",
+					JSONUtil.putAll(
+						JSONUtil.put(
+							"label", "Name"
+						).put(
+							"name", "name"
+						).put(
+							"type", "text"
+						).put(
+							"defaultValue", "Dynamically rendered Player"
+						),
+						JSONUtil.put(
+							"label", "Color"
+						).put(
+							"name", "color"
+						).put(
+							"type", "colorPalette"
+						).put(
+							"defaultValue",
+							JSONUtil.put(
+								"cssClass", "primary"
+							).put(
+								"rgbValue", "rgb(11, 95, 255);"
+							)
+						),
+						JSONUtil.put(
+							"label", "Blog Post"
+						).put(
+							"name", "blogPost"
+						).put(
+							"type", "itemSelector"
+						).put(
+							"typeOptions",
+							JSONUtil.put(
+								"enableSelectTemplate", true
+							)
+						))))
+		).toString();
+	}
+
+	@Override
 	public String getLabel(Locale locale) {
 		return "Player Renderer";
 	}
@@ -50,6 +105,43 @@ public class PlayerFragmentRenderer implements FragmentRenderer {
 		HttpServletRequest httpServletRequest,
 		HttpServletResponse httpServletResponse)
 	throws IOException {
+
+		FragmentEntryLink fragmentEntryLink = 
+			fragmentRendererContext.getFragmentEntryLink();
+
+		JSONObject configuration = null;
+		
+		try {
+			configuration = 
+				FragmentEntryConfigUtil.getConfigurationJSONObject(
+					getConfiguration(fragmentRendererContext), 
+					fragmentEntryLink.getEditableValues(),
+					fragmentRendererContext.getSegmentsExperienceIds());
+
+			String name = configuration.getString("name");
+			JSONObject color = configuration.getJSONObject("color");
+			JSONObject blogPost = configuration.getJSONObject("blogPost");
+
+			httpServletRequest.setAttribute("name", name);
+			httpServletRequest.setAttribute("color", color.getString("rgbValue"));
+
+			if (blogPost.getLong("entryId", 0) > 0) {
+				ThemeDisplay themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+				StringBundler sb = new StringBundler(4);
+
+				sb.append(themeDisplay.getPathMain());
+				sb.append("/blogs/find_entry?entryId=");
+				sb.append(blogPost.getLong("entryId"));
+				sb.append("&showAllEntries=0");
+
+				httpServletRequest.setAttribute("blogPostUrl", sb.toString());
+			}
+		}
+		catch (Exception e) {
+			throw new IOException(e);
+		}
 
 		_jspRenderer.renderJSP(
 			_servletContext, httpServletRequest, 
--- player-fragment-renderer/src/main/resources/META-INF/resources/player.jsp
+++ player-fragment-renderer/src/main/resources/META-INF/resources/player.jsp
@@ -1,7 +1,20 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
 
 <portlet:defineObjects />
 
-<div>
-    Dynamically rendered Player
+<%
+String name = (String)request.getAttribute("name");
+String color = (String)request.getAttribute("color");
+String blogPostUrl = (String)request.getAttribute("blogPostUrl");
+%>
+
+<div style="background-color: <%= color %>;">
+    <span class=""><%= name %></span>
+
+    <c:if test="<%= blogPostUrl != null %>">
+        <div class="mt-2">
+            <a href="<%= blogPostUrl %>">Blog post about <%= name %></a>
+        </div>
+    </c:if>
 </div>
